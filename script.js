8fe2518a4437bf4a848a18a656c8f60b06540b87
bree melechinsky <groundcontroltomajorbree@gmail.com>
2018-04-13 11:45:54 -0500

    free clicks (works in console

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    console.log('boom babayyy jQuery has loaded');
+    $('#consoleClicky').on('click', function(){
+        console.log('you win');
+    });
+});
+
+console.log('js too!');
+
